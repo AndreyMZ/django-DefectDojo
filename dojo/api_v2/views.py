ea5c1ba25695dcef87cfb060a92248f267a90b4b
Andrey Zelenchuk <azelenchuk@plesk.com>
2019-12-18 16:10:42 +0700

    API v2: add an ability to filter test_types by `id`, `name`, `static_tool`, `dynamic_tool` fields.

--- dojo/api_v2/views.py
+++ dojo/api_v2/views.py
@@ -1183,7 +1183,7 @@ class TestTypesViewSet(mixins.ListModelMixin,
     serializer_class = serializers.TestTypeSerializer
     queryset = Test_Type.objects.all()
     filter_backends = (DjangoFilterBackend,)
-    filter_fields = ('name',)
+    filter_fields = ('id', 'name', 'static_tool', 'dynamic_tool')
 
 
 class ToolConfigurationsViewSet(mixins.ListModelMixin,
