1b63fa0a2c43e1fd361443dbf62f3665cf187c2b
Andrey Zelenchuk <azelenchuk@plesk.com>
2020-01-31 16:25:43 +0700

    Social auth: remove unneeded `dojo.pipeline.modify_permissions` from `SOCIAL_AUTH_PIPELINE`.

--- dojo/pipeline.py
+++ dojo/pipeline.py
@@ -48,8 +48,3 @@ def social_uid(backend, details, response, *args, **kwargs):
         # This modified way needs to work
         else:
             return {'uid': response.get('preferred_username')}
-
-
-def modify_permissions(backend, uid, user=None, social=None, *args, **kwargs):
-    if kwargs.get('is_new'):
-        user.is_staff = False
