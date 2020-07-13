cff690c51c85f41954009aa79de204ea126a5103
Andrey Zelenchuk <azelenchuk@plesk.com>
2020-07-13 17:49:35 +0700

    API: add an ability to delete a test type.

--- dojo/api_v2/views.py
+++ dojo/api_v2/views.py
@@ -1173,6 +1173,7 @@ class TestTypesViewSet(mixins.ListModelMixin,
                        mixins.RetrieveModelMixin,
                        mixins.UpdateModelMixin,
                        mixins.CreateModelMixin,
+                       mixins.DestroyModelMixin,
                        viewsets.GenericViewSet):
     serializer_class = serializers.TestTypeSerializer
     queryset = Test_Type.objects.all()
