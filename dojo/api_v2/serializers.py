8347feebbad3762494734f4d872c957acdb35e56
Andrey Zelenchuk <azelenchuk@plesk.com>
2020-07-17 19:48:36 +0700

    API: remove `findings_list` from the product view, because it may be very big.

--- dojo/api_v2/serializers.py
+++ dojo/api_v2/serializers.py
@@ -389,8 +389,6 @@ class ProductMemberSerializer(serializers.ModelSerializer):
 
 class ProductSerializer(TaggitSerializer, serializers.ModelSerializer):
     findings_count = serializers.SerializerMethodField()
-    findings_list = serializers.SerializerMethodField()
-
     tags = TagListSerializerField(required=False)
     product_meta = ProductMetaSerializer(read_only=True, many=True)
 
@@ -406,9 +404,6 @@ class ProductSerializer(TaggitSerializer, serializers.ModelSerializer):
 
     def get_findings_count(self, obj):
         return obj.findings_count
-
-    def get_findings_list(self, obj):
-        return obj.open_findings_list
 
 
 class ProductTypeMemberSerializer(serializers.ModelSerializer):
