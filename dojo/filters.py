d9404e4a4deeb3ac077d69fbcd35690956e4b4e9
Andrey Zelenchuk <azelenchuk@plesk.com>
2021-01-26 14:16:14 +0700

    API: add an ability to filter endpoints by `protocol`, `path`.

--- dojo/filters.py
+++ dojo/filters.py
@@ -1720,7 +1720,13 @@ class ApiEndpointFilter(DojoFilter):
 
     class Meta:
         model = Endpoint
-        fields = ['id', 'host', 'product']
+        fields = {
+            'id': ['exact'],
+            'protocol': ['exact', 'isnull'],
+            'host': ['exact', 'isnull'],
+            'path': ['exact', 'isnull'],
+            'product': ['exact'],
+        }
 
 
 class EngagementTestFilter(DojoFilter):
