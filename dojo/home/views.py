2a811ab429e3804aea405880c1f22874122e46e4
Andrey Zelenchuk <azelenchuk@plesk.com>
2021-01-30 14:26:42 +0700

    Refactor: `dojo.home.views`. (No semantic changes.)

--- dojo/home/views.py
+++ dojo/home/views.py
@@ -49,40 +49,39 @@ def dashboard(request):
 
     severity_count_all = get_severities_all(findings)
     severity_count_by_month = get_severities_by_month(findings, today)
+    punchcard, ticks = get_punchcard_data(findings, today - relativedelta(weeks=26), 26)
 
-    start_date = today - relativedelta(weeks=26)
-    punchcard, ticks = get_punchcard_data(findings, start_date, 26)
-
-    unassigned_surveys = Answered_Survey.objects.all().filter(
-        assignee_id__isnull=True, completed__gt=0)
+    unassigned_surveys = Answered_Survey.objects.filter(assignee_id__isnull=True, completed__gt=0)
 
     add_breadcrumb(request=request, clear=True)
-    return render(request,
-                  'dojo/dashboard.html',
-                  {'engagement_count': engagement_count,
-                   'finding_count': finding_count,
-                   'mitigated_count': mitigated_count,
-                   'accepted_count': accepted_count,
-                   'critical': severity_count_all['Critical'],
-                   'high': severity_count_all['High'],
-                   'medium': severity_count_all['Medium'],
-                   'low': severity_count_all['Low'],
-                   'info': severity_count_all['Info'],
-                   'by_month': severity_count_by_month,
-                   'punchcard': punchcard,
-                   'ticks': ticks,
-                   'surveys': unassigned_surveys})
+    return render(request, 'dojo/dashboard.html', {
+        'engagement_count': engagement_count,
+        'finding_count': finding_count,
+        'mitigated_count': mitigated_count,
+        'accepted_count': accepted_count,
+        'critical': severity_count_all['Critical'],
+        'high': severity_count_all['High'],
+        'medium': severity_count_all['Medium'],
+        'low': severity_count_all['Low'],
+        'info': severity_count_all['Info'],
+        'by_month': severity_count_by_month,
+        'punchcard': punchcard,
+        'ticks': ticks,
+        'surveys': unassigned_surveys,
+    })
 
 
 def get_severities_all(findings):
     severities_all = findings.values('severity').annotate(count=Count('severity')).order_by()
 
-    # make sure all keys are present
-    sev_counts_all = {'Critical': 0,
-                  'High': 0,
-                  'Medium': 0,
-                  'Low': 0,
-                  'Info': 0}
+    # Make sure all keys are present.
+    sev_counts_all = {
+        'Critical': 0,
+        'High':     0,
+        'Medium':   0,
+        'Low':      0,
+        'Info':     0,
+    }
 
     for s in severities_all:
         sev_counts_all[s['severity']] = s['count']
@@ -91,37 +90,25 @@ def get_severities_all(findings):
 
 
 def get_severities_by_month(findings, today):
-    by_month = list()
+    severities_by_month = findings\
+        .filter(created__date__gte=(today - relativedelta(months=6)))\
+        .values('created__year', 'created__month', 'severity')\
+        .annotate(count=Count('severity'))\
+        .order_by()
 
-    # order_by is needed due to ordering being present in Meta of Finding
-    # severities_all = findings.values('severity').annotate(count=Count('severity')).order_by()
-    severities_by_month = findings.filter(created__gte=today + relativedelta(months=-6)) \
-                                .values('created__year', 'created__month', 'severity').annotate(count=Count('severity')).order_by('created__year', 'created__month')
+    # Ths chart expects a, b, c, d, e instead of Critical, High, ...
+    SEVERITY_MAP = {
+        'Critical': 'a',
+        'High':     'b',
+        'Medium':   'c',
+        'Low':      'd',
+        'Info':     'e',
+    }
 
     results = {}
     for ms in severities_by_month:
-        year = str(ms['created__year'])
-        month = str(ms['created__month']).zfill(2)
-        key = year + '-' + month
+        key = f"{ms['created__year']}-{ms['created__month']:02}"
+        month_stats = results.setdefault(key, {'y': key, 'a': 0, 'b': 0, 'c': 0, 'd': 0, 'e': 0})
+        month_stats[SEVERITY_MAP[ms['severity']]] = ms['count']
 
-        if key not in results:
-            # graph expects a, b, c, d, e instead of Critical, High, ...
-            sourcedata = {'y': key, 'a': 0, 'b': 0,
-                    'c': 0, 'd': 0, 'e': 0}
-            results[key] = sourcedata
-
-        month_stats = results[key]
-
-        if ms['severity'] == 'Critical':
-            month_stats['a'] = ms['count']
-        elif ms['severity'] == 'High':
-            month_stats['b'] = ms['count']
-        elif ms['severity'] == 'Medium':
-            month_stats['c'] = ms['count']
-        elif ms['severity'] == 'Low':
-            month_stats['d'] = ms['count']
-        elif ms['severity'] == 'Info':
-            month_stats['e'] = ms['count']
-
-    by_month = [v for k, v in sorted(results.items())]
-    return by_month
+    return [v for k, v in sorted(results.items())]
