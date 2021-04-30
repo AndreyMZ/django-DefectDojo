73a31918507ab7c398a5a290b9ce02be33dc35e4
Andrey Zelenchuk <azelenchuk@plesk.com>
2021-04-30 16:21:09 +0700

    On edit finding stop calculating statuses (`active`, `false_p`, `out_of_scope`, `mitigated`) from each other.

--- dojo/finding/helper.py
+++ dojo/finding/helper.py
@@ -39,16 +39,13 @@ def pre_save_finding_status_change(sender, instance, changed_fields=None, **kwar
 
     logger.debug('%i: changed status fields pre_save: %s', instance.id or 0, changed_fields)
 
-    for field, (old, new) in changed_fields.items():
-        logger.debug("%i: %s changed from %s to %s" % (instance.id or 0, field, old, new))
-        user = None
-        if get_current_user() and get_current_user().is_authenticated:
-            user = get_current_user()
-        update_finding_status(instance, user, changed_fields)
+    current_user = get_current_user()
+    user = current_user if current_user.is_authenticated else None
+    update_finding_status(instance, user, changed_fields)
 
 
 # also get signal when id is set/changed so we can process new findings
-pre_save_changed.connect(pre_save_finding_status_change, sender=Finding, fields=['id', 'active', 'verfied', 'false_p', 'is_mitigated', 'mitigated', 'mitigated_by', 'out_of_scope', 'risk_accepted'])
+pre_save_changed.connect(pre_save_finding_status_change, sender=Finding, fields=['id', 'active', 'verfied', 'false_p', 'is_mitigated', 'mitigated', 'mitigated_by', 'out_of_scope', 'risk_accepted', 'duplicate'])
 # pre_save_changed.connect(pre_save_finding_status_change, sender=Finding)
 # post_save_changed.connect(pre_save_finding_status_change, sender=Finding, fields=['active', 'verfied', 'false_p', 'is_mitigated', 'mitigated', 'mitigated_by', 'out_of_scope'])
 
@@ -60,65 +57,17 @@ def update_finding_status(new_state_finding, user, changed_fields=None):
 
     is_new_finding = not changed_fields or (changed_fields and len(changed_fields) == 1 and 'id' in changed_fields)
 
-    # activated
-    # reactivated
-    # closed / mitigated
-    # false positivized
-    # out_of_scopified
-    # marked as duplicate
-    # marked as original
-
     if is_new_finding or 'is_mitigated' in changed_fields:
-        # finding is being mitigated
         if new_state_finding.is_mitigated:
-            # when mitigating a finding, the meta fields can only be editted if allowed
-            logger.debug('finding being mitigated, set mitigated and mitigated_by fields')
-
-            if can_edit_mitigated_data(user):
-                # only set if it was not already set by user
-                # not sure if this check really covers all cases, but if we make it more strict
-                # it will cause all kinds of issues I believe with new findings etc
-                new_state_finding.mitigated = new_state_finding.mitigated or now
-                new_state_finding.mitigated_by = new_state_finding.mitigated_by or user
-
-        # finding is being "un"mitigated
-        else:
-            new_state_finding.mitigated = None
-            new_state_finding.mitigated_by = None
-
-    # people may try to remove mitigated/mitigated_by by accident
-    if new_state_finding.is_mitigated:
-        new_state_finding.mitigated = new_state_finding.mitigated or now
-        new_state_finding.mitigated_by = new_state_finding.mitigated_by or user
-
-    if is_new_finding or 'active' in changed_fields:
-        # finding is being (re)activated
-        if new_state_finding.active:
-            new_state_finding.false_p = False
-            new_state_finding.out_of_scope = False
-            new_state_finding.is_mitigated = False
-            new_state_finding.mitigated = None
-            new_state_finding.mitigated_by = None
-        else:
-            # finding is being deactivated
-            pass
-
-    if is_new_finding or 'verified' in changed_fields:
-        pass
-
-    if is_new_finding or 'false_p' in changed_fields or 'out_of_scope' in changed_fields:
-        # existing behaviour is that false_p or out_of_scope implies mitigated
-        if new_state_finding.false_p or new_state_finding.out_of_scope:
             new_state_finding.mitigated = new_state_finding.mitigated or now
             new_state_finding.mitigated_by = new_state_finding.mitigated_by or user
-            new_state_finding.is_mitigated = True
-            new_state_finding.active = False
-            new_state_finding.verified = False
+        else:
+            new_state_finding.mitigated = None
+            new_state_finding.mitigated_by = None
 
-    # always reset some fields if the finding is not a duplicate
-    if not new_state_finding.duplicate:
-        new_state_finding.duplicate = False
-        new_state_finding.duplicate_finding = None
+    if is_new_finding or ('duplicate' in changed_fields):
+        if not new_state_finding.duplicate:
+            new_state_finding.duplicate_finding = None
 
     new_state_finding.last_status_update = now
 
